462b7cb0f4cbaf0e8bfe25a84191e788941c10a4
Andrew Arensburger <arensb>
2023-03-17 11:35:01 -0400

    Rename "dataset" to "filesystem" in the code as well.

--- plugins/modules/filesystem.py
+++ plugins/modules/filesystem.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python
 __metaclass__ = type
 
-# Manage ZFS datasets.
+# Manage ZFS filesystems.
 
 # XXX
 DOCUMENTATION = '''
 ---
-module: dataset
-short_description: Manage ZFS datasets
+module: filesystem
+short_description: Manage ZFS filesystems
 description:
-  - Create, delete, and manage ZFS datasets.
+  - Create, delete, and manage ZFS filesystems.
 options:
   comment:
     description:
-      - Comment attached to the dataset.
+      - Comment attached to the filesystem.
     type: str
   name:
     description:
-      - Name of the dataset.
+      - Name of the filesystem.
     type: str
     required: true
   state:
     description:
-      - Whether the dataset should exist or not.
+      - Whether the filesystem should exist or not.
     type: str
     choices: [ absent, present ]
     default: present
@@ -31,21 +31,21 @@ options:
 # XXX
 EXAMPLES = '''
 - name: Create a filesystem
-  arensb.truenas.dataset:
+  arensb.truenas.filesystem:
     name: pool0/mydata
 
 - name: Delete a filesystem
-  arensb.truenas.dataset:
+  arensb.truenas.filesystem:
     name: pool0/mydata
     state: absent
 '''
 
 # XXX
 RETURN = '''
-dataset:
+filesystem:
   description:
     - A data structure describing the characteristics of a newly-created
-      dataset.
+      filesystem.
   type: dict
 '''
 
@@ -57,15 +57,15 @@ from ansible_collections.arensb.truenas.plugins.module_utils.middleware \
 def main():
     # XXX - from pool.dataset.create:
     # x name (str)
-    # - type: {FILESYSTEM, VOLUME}
-    # - volsize (int)
+    # ! type: {FILESYSTEM, VOLUME}
+    # ! volsize (int)
     #   required for type==VOLUME. Should be a multiple of block size.
-    # - volblocksize: {512, 1K, 2K, 4K, 8K, 16K, 32K, 64K, 128K}
+    # ! volblocksize: {512, 1K, 2K, 4K, 8K, 16K, 32K, 64K, 128K}
     #   Only used for type=VOLUME
-    # - sparse (bool)
+    # ! sparse (bool)
     #   Only used for type=VOLUME
     # - force_size (bool)
-    # - comments (str)
+    # x comments (str)
     # - sync: {STANDARD, ALWAYS, DISABLED}
     # - compression: {OFF, LZ4, GZIP, GZIP-1, GZIP-9, ZSTD, LZJB, ...}
     # - atime: {ON, OFF}
@@ -122,32 +122,32 @@ def main():
     state = module.params['state']
     comment = module.params['comment']
 
-    # Look up the dataset.
+    # Look up the filesystem.
     try:
         # pool.dataset.query has both "id" and "name", which are
         # always the same, it looks like. I think "id" is the
         # middleware database identifier, while "name" is what you'd
         # use while managing ZFS.
-        dataset_info = mw.call("pool.dataset.query",
-                               [["name", "=", name]])
-        if len(dataset_info) == 0:
-            # No such dataset
-            dataset_info = None
+        fs_info = mw.call("pool.dataset.query",
+                          [["name", "=", name]])
+        if len(fs_info) == 0:
+            # No such filesystem
+            fs_info = None
         else:
-            # Dataset exists
-            dataset_info = dataset_info[0]
+            # Filesystem exists
+            fs_info = fs_info[0]
 
     except Exception as e:
-        module.fail_json(msg=f"Error looking up dataset {name}: {e}")
+        module.fail_json(msg=f"Error looking up filesystem {name}: {e}")
 
-    result['info'] = dataset_info
+    result['info'] = fs_info
 
-    # First, check whether the dataset even exists.
-    if dataset_info is None:
-        # Dataset doesn't exist
+    # First, check whether the filesystem even exists.
+    if fs_info is None:
+        # Filesystem doesn't exist
 
         if state == 'present':
-            # Dataset is supposed to exist, so create it.
+            # Filesystem is supposed to exist, so create it.
 
             # Collect arguments to pass to dataset.create()
             arg = {
@@ -158,35 +158,35 @@ def main():
                 arg['comment'] = comment
 
             if module.check_mode:
-                result['msg'] = f"Would have created dataset {name} with {arg}"
+                result['msg'] = f"Would have created filesystem {name} with {arg}"
             else:
                 #
-                # Create new dataset
+                # Create new filesystem
                 #
                 try:
                     err = mw.call("pool.dataset.create", arg)
                     result['msg'] = err
                 except Exception as e:
                     result['failed_invocation'] = arg
-                    module.fail_json(msg=f"Error creating dataset {name}: {e}")
+                    module.fail_json(msg=f"Error creating filesystem {name}: {e}")
 
                 # Returns a long data structure. Looks like the same
                 # one that pool.dataset.create() takes, or that
                 # pool.dataset.update() returns (I haven't compared
                 # them).
                 # Looks interesting. Return it.
-                result['dataset'] = err
+                result['filesystem'] = err
 
             result['changed'] = True
         else:
-            # Dataset is not supposed to exist.
+            # Filesystem is not supposed to exist.
             # All is well
             result['changed'] = False
 
     else:
-        # Dataset exists
+        # Filesystem exists
         if state == 'present':
-            # Dataset is supposed to exist
+            # Filesystem is supposed to exist
 
             # Make list of differences between what is and what should
             # be.
@@ -211,45 +211,45 @@ def main():
             # How to delete the comment entirely?
 
             if comment is not None:
-                if "comments" not in dataset_info or \
-                   "rawvalue" not in dataset_info['comments'] or \
-                   dataset_info['comments']['rawvalue'] != comment:
+                if "comments" not in fs_info or \
+                   "rawvalue" not in fs_info['comments'] or \
+                   fs_info['comments']['rawvalue'] != comment:
                     arg['comments'] = comment
 
-            # If there are any changes, dataset.update()
+            # If there are any changes, filesystem.update()
             if len(arg) == 0:
                 # No changes
                 result['changed'] = False
             else:
                 #
-                # Update dataset.
+                # Update filesystem.
                 #
                 if module.check_mode:
-                    result['msg'] = f"Would have updated dataset {name}: {arg}"
+                    result['msg'] = f"Would have updated filesystem {name}: {arg}"
                 else:
                     try:
                         err = mw.call("pool.dataset.update",
-                                      dataset_info['id'],
+                                      fs_info['id'],
                                       arg)
                     except Exception as e:
-                        module.fail_json(msg=f"Error updating dataset {name} with {arg}: {e}")
+                        module.fail_json(msg=f"Error updating filesystem {name} with {arg}: {e}")
                         # Return any interesting bits from err
                         result['status'] = err['status']
                 result['changed'] = True
         else:
-            # Dataset is not supposed to exist
+            # Filesystem is not supposed to exist
 
             if module.check_mode:
-                result['msg'] = f"Would have deleted dataset {name}"
+                result['msg'] = f"Would have deleted filesystem {name}"
             else:
                 try:
                     #
-                    # Delete dataset.
+                    # Delete filesystem.
                     #
                     err = mw.call("pool.dataset.delete",
-                                  dataset_info['id'])
+                                  fs_info['id'])
                 except Exception as e:
-                    module.fail_json(msg=f"Error deleting dataset {name}: {e}")
+                    module.fail_json(msg=f"Error deleting filesystem {name}: {e}")
             result['changed'] = True
 
     module.exit_json(**result)
